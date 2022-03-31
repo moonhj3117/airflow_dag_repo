395c1f80c9df04c5ab7a90ce4cfdebf244e8cf78
moonhj3117 <moonhj3117@gmail.com>
2022-03-31 13:29:18 +0900

    Create bach_oper.py

---
+++ dags/bach_oper.py
@@ -0,0 +1,48 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow import dag
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import BranchPythonOperator
+from airflow.utils.trigger_rule import  TriggerRule
+
+def random_branch_path():
+    from random import randint
+
+    return "path1" if randint(1, 2) == 1 else "my_name_en"
+
+with DAG(**dag_args) as dag:
+    t1 = BashOperator(
+        task_id = 'print_date',
+        bash_command = 'date',
+    )
+
+    t2 = BranchPythonOperator(
+        task_id = 'branch',
+        python_callable=random_branch_path(),
+    )
+
+    t3 = BashOperator(
+        task_id='my_name_ko',
+        depends_on_past=False,
+        bash_command='echo "안녕하세요"',
+    )
+
+    t4 = BashOperator(
+        task_id= 'my_name_en',
+        depends_on_past=False,
+        bash_command='echo "HI"',
+    )
+
+    complete = BashOperator(
+        task_id='complete',
+        depends_on_past=False,
+        bash_command='echo "complete!~"',
+        trigger_rule=TriggerRule.NONE_FAILED
+    )
+
+    dummy_1 = DummyOperator(task_id="path1")
+
+    t1 >> t2 >> dummy_1 >> complete
+    t1 >> t2 >> t4 >> complete
