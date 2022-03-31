f4b80b07cca9aedc88466fcef7a3b205806dfd2d
moonhj3117 <moonhj3117@gmail.com>
2022-03-31 11:39:02 +0900

    Update cat-test.py

--- dags/cat-test.py
+++ dags/cat-test.py
@@ -22,19 +22,19 @@ with DAG(
         bash_command='echo 1',
     )
 
-    # #### cat txt file  --> 텍스트 파일을 읽습니다
-    # read_text_file_command = f'cd {text_file_path} && cat test.txt'
-    # read_text_file = BashOperator(
-    #     task_id='cat_text_file',
-    #     bash_command=read_text_file_command,
-    #     dag=dag)
-    #
-    # #### remove txt file  --> 텍스트 파일을 삭제합니다.
-    # remove_text_file_command = f'cd {text_file_path} && rm test.txt'
-    # remove_text_file = BashOperator(
-    #     task_id='remove_text_file',
-    #     bash_command=remove_text_file_command,
-    #     dag=dag)
-    #
-    # create_text_file >> read_text_file >> remove_text_file  # 이것은 위의 task를 이어주는 줄입니다.
-    # create_text_file
+    #### cat txt file  --> 텍스트 파일을 읽습니다
+    read_text_file_command = f'cd {text_file_path} && cat test.txt'
+    read_text_file = BashOperator(
+        task_id='cat_text_file',
+        bash_command=read_text_file_command,
+        dag=dag)
+
+    #### remove txt file  --> 텍스트 파일을 삭제합니다.
+    remove_text_file_command = f'cd {text_file_path} && rm test.txt'
+    remove_text_file = BashOperator(
+        task_id='remove_text_file',
+        bash_command=remove_text_file_command,
+        dag=dag)
+
+    create_text_file >> read_text_file >> remove_text_file  # 이것은 위의 task를 이어주는 줄입니다.
+    create_text_file
